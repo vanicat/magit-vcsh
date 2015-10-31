5fb0fa4a9079ed7ab45e5ff96f224ff6d954843c
Rémi Vanicat <vanicat@debian.org>
2015-10-31 16:09:43 +0100

    correct header.
    Magit-vcsh is now an emacs package. It can be installed with
    `package-install-file' for example.

--- magit-vcsh.el
+++ magit-vcsh.el
@@ -2,12 +2,19 @@
 
 ;; Copyright (C) 2013, 2015 Rémi Vanicat
 
-;; Magit is free software; you can redistribute it and/or modify it
+;; Author: Rémi Vanicat <vanicat@debian.org>
+
+;; Package-Requires: ((emacs "24.4") (magit "2.2.2"))
+;; Keywords: tools
+;; Homepage: https://github.com/magit/magit
+;; Version: 0.2
+
+;; Magit-vcsh is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 ;;
-;; Magit is distributed in the hope that it will be useful, but WITHOUT
+;; Magit-vcsh is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 ;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
 ;; License for more details.
@@ -124,3 +131,5 @@ if NAME not nil, it is a name of a vcsh repos"
 (advice-add 'magit-process-setup :around 'magit-vcsh-set-env-advice)
 
 (provide 'magit-vcsh)
+
+;;; magit-vcsh.el ends here
